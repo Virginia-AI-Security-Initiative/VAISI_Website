ac65650aac877a3a78965409ab348a939d5c14e8
slifland <sethlifland11@gmail.com>
2026-09-05 08:12:05 -0400

    added email

--- lib/admin/email-seeds.ts
+++ lib/admin/email-seeds.ts
@@ -140,6 +140,46 @@ Click here to unsubscribe: ${unsubscribeUrl}`,
     style_weight: 'primary',
     source_notes: 'Confirmed sent by the user via screenshot. The subject was not visible and is retained from the preceding draft.',
   },
+  {
+    slug: 'fellowship-applications-close-tomorrow-2026',
+    subject: 'VAISI Fellowship Applications Close Tomorrow!',
+    body: `Hello everyone,
+
+**Reminder that VAISI fellowship [applications](https://airtable.com/appM8XoHX2voW3LQe/pag7pdRcPx9uhaRnF/form) close tomorrow (September 6) at 11:59 PM EDT!**
+
+We are running an Intro to AI Safety fellowship and an AI Policy fellowship. They require about two hours per week for eight and ten weeks, respectively. Both have an optional 2-week capstone project at the end. They consist of reading, discussion, activities, and free food! You can see testimonials from some of our Spring fellowship graduates [here](https://vaisi.org/get-involved#fellowship-testimonials-heading).
+
+Learn more about the topics covered [here](https://docs.google.com/document/d/10HcHGxPJ5VpnWoW3LQe/edit?usp=sharing), and [apply here](https://airtable.com/appM8XoHX2voW3LQe/pag7pdRcPx9uhaRnF/form). Note that there is only one application which covers both fellowships. I’ve put an FAQ at the bottom of this email, and if you have any questions not answered there, feel free to reach out to me or any VAISI officer.
+
+Thanks,
+Seth Lifland
+
+[Join our GroupMe](https://groupme.com/join_group/110490963/bxseYw8L)
+[Follow our Instagram](https://www.instagram.com/vaisi_atuva/)
+[Learn more at vaisi.org](http://vaisi.org/)
+[Click here to unsubscribe](${unsubscribeUrl})
+
+**FAQ**
+
+Q: Which fellowship should I apply to? What is the difference between the intro and policy fellowships?
+A: The Intro to AI Safety fellowship is broader and more technical. The policy fellowship is a better fit if you are already planning to enter public policy or governance with your career and will spend more time focusing on topics like (federal- and state-level) regulation and geopolitics. The policy fellowship is also two weeks longer. As a reminder, you can see curriculum outlines [here](https://docs.google.com/document/d/10HcHGxPJ5VpnWoW3LQe/edit?tab=t.0#heading=h.423o2d85ugw).
+
+Q: When will I hear back?
+A: We are hoping to send out next steps on Monday, September 7. If you haven’t heard anything from us by Wednesday, September 9, feel free to reach out.
+
+Q: What does a fellowship meeting look like?
+A: A typical meeting involves dinner, content (typically readings or watching videos), discussion, and relevant activities. We will adjust ratios and formats as the fellowship goes based on what our fellows are finding most useful.
+
+Q: Can I do both fellowships?
+A: You are welcome to indicate somewhere on your application that you want to participate in both fellowships, and we will consider you for this. Note that there is some duplicated content in the first couple weeks, but the rest of the content will be mostly disjoint. We also ask that you seriously consider this time commitment of 4 hours per week before asking to do both this semester.`,
+    distribution_context: 'announcement_list',
+    audience: 'VAISI announcement list',
+    recipient: 'vaisi_announcements',
+    status: 'sent',
+    sent_date: '2026-09-05',
+    style_weight: 'primary',
+    source_notes: 'User-provided final announcement email. Rich-text nonbreaking spaces and pasted line-break artifacts were normalized.',
+  },
 ];
 
 const introFellowshipSyllabus =
